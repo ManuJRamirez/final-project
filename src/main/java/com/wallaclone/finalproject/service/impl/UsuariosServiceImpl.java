e24ece610ea7341d4a86f97b470a5fb7b0e8df57
Manuel Ramirez <manramloz@gmail.com>
2024-03-14 21:38:15 +0100

    signUp con codificacion de contraseña

--- src/main/java/com/wallaclone/finalproject/service/impl/UsuariosServiceImpl.java
+++ src/main/java/com/wallaclone/finalproject/service/impl/UsuariosServiceImpl.java
@@ -2,6 +2,7 @@ package com.wallaclone.finalproject.service.impl;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.wallaclone.finalproject.dto.SignUpResponseDto;
@@ -18,10 +19,14 @@ public class UsuariosServiceImpl implements UsuariosService {
 
 	@Autowired
 	ModelMapper modelMapper;
-
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
 	@Override
 	public SignUpResponseDto signUp(UsuarioDto signUpBodyDto) throws Exception {
 
+		signUpBodyDto.setContrasenia(passwordEncoder.encode(signUpBodyDto.getContrasenia()));
 		usuariosRepository.save(modelMapper.map(signUpBodyDto, Usuario.class));
 		return null;
 	}
