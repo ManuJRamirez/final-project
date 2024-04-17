a515577a758f2d7bfce55b9d7e7decdafadc3ccd
Manuel Ramirez <manramloz@gmail.com>
2024-04-18 01:03:35 +0200

    fix corps x2

--- src/main/java/com/wallaclone/finalproject/config/CorsConfig.java
+++ src/main/java/com/wallaclone/finalproject/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500", "http://13.50.250.169", "*") // Reemplaza esto con el origen de tu frontend
+                        .allowedOrigins("http://127.0.0.1:5500", "http://13.50.250.169") // Reemplaza esto con el origen de tu frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Especifica los métodos HTTP permitidos
                         .allowedHeaders("*") // Permite todos los encabezados
                         .allowCredentials(true); // Permitir credenciales
